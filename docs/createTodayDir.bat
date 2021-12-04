27f3b5fd6db6c7ef4a6167eb9f8c680559d4b0a5
cat2151 <fct_2004@mail.goo.ne.jp>
2021-12-05 00:54:07 +0900

    add createTodayDir.bat

---
+++ docs/createTodayDir.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+set BASE=.\
+set TODAY=%date:~0,4%%date:~5,2%%date:~8,2%
+
+rem copy
+xcopy /ID %BASE%_template %BASE%%TODAY%
+
+rem watch
+start npm run watch_games
+
+rem browser
+start http://localhost:4000?%TODAY%
+
+rem vscode
+code %BASE%%TODAY%\main.js
